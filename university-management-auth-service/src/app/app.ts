3ea026515fd8e528f5b4bbf02ad6a3bd0527b1bd
Raiyan <tajkierhaque@gmail.com>
2023-12-14 15:51:33 +0600

    eslint and prettier setup done

--- university-management-auth-service/src/app/app.ts
+++ university-management-auth-service/src/app/app.ts
@@ -1,8 +1,8 @@
-import express, { Application, Request, Response } from "express";
-const app: Application = express();
+import express, { Application, Request, Response } from 'express'
+const app: Application = express()
 
-app.get("/", (req: Request, res: Response) => {
-  res.status(200).json({ baseUrl: "http://localhost:5000/", health: "good" });
-});
+app.get('/', (req: Request, res: Response) => {
+  res.status(200).json({ baseUrl: 'http://localhost:5000/', health: 'good' })
+})
 
-export default app;
+export default app
--- university-management-auth-service/src/server.ts
+++ university-management-auth-service/src/server.ts
@@ -1,20 +1,20 @@
-import http from "http";
-import mongoose from "mongoose";
-import app from "./app/app";
-import config from "./config";
+import http from 'http'
+import mongoose from 'mongoose'
+import app from './app/app'
+import config from './config'
 
-const server = http.createServer(app);
+const server = http.createServer(app)
 
 async function bootstrap() {
   try {
-    await mongoose.connect(config.db_url as string);
-    console.log("DB connected Successfully");
+    await mongoose.connect(config.db_url as string)
+    console.log('DB connected Successfully')
     server.listen(config.port, () => {
-      console.log(`Example app listening on port ${config.port}`);
-    });
+      console.log(`Example app listening on port ${config.port}`)
+    })
   } catch (error) {
-    console.log(error);
+    console.log(error)
   }
 }
 
-bootstrap();
+bootstrap()
